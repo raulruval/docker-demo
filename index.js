14a04edc30bed6d8efdd702afc7e82b0c084bae8
raulruval <32712264+raulruval@users.noreply.github.com>
2021-04-24 20:35:38 -0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola Mundo!');
+  res.send('Hola Mundo, test!');
 });
 
 var server = app.listen(3000, function () {
