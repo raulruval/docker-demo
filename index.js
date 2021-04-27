59ee49ba3b3acf812dac778f9dc784eb6fe76ae6
raulruval <32712264+raulruval@users.noreply.github.com>
2021-04-27 09:43:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola Mundo, probando github hooks!');
+  res.send('Probando, probando');
 });
 
 var server = app.listen(3000, function () {
