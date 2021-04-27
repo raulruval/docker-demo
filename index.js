a5d3bdca450550e8ad5eefc95217c7b0d3322754
raulruval <32712264+raulruval@users.noreply.github.com>
2021-04-27 12:34:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Probando para el vídeo de youtube Raúl Ruiz');
+  res.send('Probando para el vídeo 2');
 });
 
 var server = app.listen(3000, function () {
